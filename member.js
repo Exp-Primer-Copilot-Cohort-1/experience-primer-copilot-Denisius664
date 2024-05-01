7bee49f8d95506a52f0fb657e6bf9c8ed52b29e6
Denis <51728543+Denisius664@users.noreply.github.com>
2024-05-01 15:19:10 +0000

    Third commit

---
+++ member.js
@@ -0,0 +1,42 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: "HTML",
+            description: "HTML stands for Hyper Text Markup Language. It is used for structuring a webpage and its content.",
+            image: "https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png",
+            link: "https://www.w3schools.com/html/"
+        },
+        {
+            name: "CSS",
+            description: "CSS stands for Cascading Style Sheets. It is used for styling a webpage and its content.",
+            image: "https://www.w3.org/Style/CSS/logos/css3.png",
+            link: "https://www.w3schools.com/css/"
+        },
+        {
+            name: "JavaScript",
+            description: "JavaScript is a programming language that is used to create interactive effects within a webpage.",
+            image: "https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png",
+            link: "https://www.w3schools.com/js/"
+        },
+        {
+            name: "React",
+            description: "React is a JavaScript library used for building user interfaces.",
+            image: "https://cdn.auth0.com/blog/react-js/react.png",
+            link: "https://reactjs.org/"
+        },
+        {
+            name: "Node.js",
+            description: "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
+            image: "https://nodejs.org/static/images/logos/nodejs-new-pantone-black.png",
+            link: "https://nodejs.org/"
+        },
+        {
+            name: "MongoDB",
+            description: "MongoDB is a NoSQL database program.",
+            image: "https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png",
+            link: "https://www.mongodb.com/"
+        }
+    ];
+
+    return skills;
+}
